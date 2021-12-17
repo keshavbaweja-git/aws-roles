d020cdd021308a6f69805ddbcb11ed717f5d91ce
Keshav <keshavkb@amazon.com>
2021-12-17 18:26:28 +0800

    Created roles

--- src/main/java/com/myorg/AwsRolesStack.java
+++ src/main/java/com/myorg/AwsRolesStack.java
@@ -1,10 +1,16 @@
 package com.myorg;
 
-import software.constructs.Construct;
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.iam.*;
+import software.constructs.Construct;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static software.amazon.awscdk.services.iam.Effect.*;
 
 public class AwsRolesStack extends Stack {
     public AwsRolesStack(final Construct scope, final String id) {
@@ -13,12 +19,49 @@ public class AwsRolesStack extends Stack {
 
     public AwsRolesStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
-
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsRolesQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        final Role ssmManagedInstanceRole = Role.Builder
+                .create(this, "ssmManagedInstanceRole")
+                .roleName("SSMManagedInstanceRole")
+                .assumedBy(new ServicePrincipal("ec2.amazonaws.com"))
+                .managedPolicies(asList(ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")))
+                .build();
+        CfnOutput.Builder
+                .create(this, "ssmManagedInstanceRoleArn")
+                .exportName("SSMManagedInstanceRoleArn")
+                .value(ssmManagedInstanceRole.getRoleArn())
+                .build();
+        final PolicyDocument lakeFormationWorkflowPolicyDocument = PolicyDocument.Builder
+                .create()
+                .statements(asList(PolicyStatement
+                                .Builder
+                                .create()
+                                .effect(ALLOW)
+                                .actions(asList("lakeformation:GetDataAccess",
+                                        "lakeformation:GrantPermissions"))
+                                .resources(asList("*"))
+                                .build(),
+                        PolicyStatement
+                                .Builder
+                                .create()
+                                .effect(ALLOW)
+                                .actions(asList("iam:PassRole"))
+                                .resources(asList("arn:aws:iam::563361968771:role/LakeFormationWorkflowRole"))
+                                .build()))
+                .build();
+        final Map<String, PolicyDocument> lakeFormationWorkflowPolicyDocuments = new HashMap<>();
+        lakeFormationWorkflowPolicyDocuments.put("LakeFormationWorkflow",
+                lakeFormationWorkflowPolicyDocument);
+        final Role lakeFormationWorkflowRole = Role.Builder
+                .create(this, "lakeFormationWorkflowRole")
+                .roleName("LakeFormationWorkflowRole2")
+                .assumedBy(new ServicePrincipal("glue.amazonaws.com"))
+                .managedPolicies(asList(ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSGlueServiceRole")))
+                .inlinePolicies(lakeFormationWorkflowPolicyDocuments)
+                .build();
+        CfnOutput.Builder
+                .create(this, "lakeFormationWorkflowRoleArn")
+                .exportName("LakeFormationWorkflowRoleArn")
+                .value(lakeFormationWorkflowRole.getRoleArn())
+                .build();
     }
 }
